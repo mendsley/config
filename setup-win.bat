7e2265d2d9370991ed448c7d05e868d9c734ba00
Matthew Endsley <mendsley@gmail.com>
2012-02-04 01:16:49 -0800

    fixing setup-win

--- setup-win.bat
+++ setup-win.bat
@@ -5,11 +5,12 @@ setlocal
 	set "HOME=%USERPROFILE%"
 
 	:: vim/gvim configuration
-	del %HOME%\_gvimrc
-	del %HOME%\_vimrc
-	del %HOME%\.gvimrc
-	rmdir /s /q %HOME%\.vim
-	rmdir /s /q %HOME%\vimfiles
+	del %HOME%\.vimrc 1>nul 2>nul
+	del %HOME%\.gvimrc 1>nul 2>nuk
+	del %HOME%\_gvimrc 1>nul 2>nul
+	del %HOME%\_vimrc 1>nul 2>nul
+	rmdir /s /q %HOME%\.vim 1>nul 2>nul
+	rmdir /s /q %HOME%\vimfiles 1>nul 2>nul
 	mklink %HOME%\.vimrc "%ROOT%\vim\.vimrc"
 	mklink %HOME%\.gvimrc "%ROOT%\vim\.gvimrc"
 	mklink /D %HOME%\.vim "%ROOT%\vim"
