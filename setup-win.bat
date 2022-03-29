5cc78f6c57d33e748bf3356373fd725637f6fdcb
Matthew Endsley <mendsley@gmail.com>
2022-03-28 18:54:36 -0700

    Update github SSH keys

--- setup-win.bat
+++ setup-win.bat
@@ -11,7 +11,6 @@ setlocal
 	del %HOME%\.gvimrc 1>nul 2>nul
 	del %HOME%\_gvimrc 1>nul 2>nul
 	del %HOME%\_vimrc 1>nul 2>nul
-	del %HOME%\.ssh\config 1>nul 2>nul
 	rmdir /s /q %HOME%\.vim 1>nul 2>nul
 	rmdir /s /q %HOME%\vimfiles 1>nul 2>nul
 	mklink %HOME%\.vimrc "%ROOT%\vim\.vimrc"
@@ -20,8 +19,9 @@ setlocal
 	mklink %HOME%\_vimrc "%HOME%\.vimrc"
 	mklink /D %HOME%\vimfiles "%HOME%\.vim"
 
+	del %HOME%\.ssh\config 1>nul 2>nul
 	mkdir %HOME%\.ssh 1>nul 2>nul
-	mklink /D %HOME%\.ssh\config %ROOT%\ssh_config
+	mklink %HOME%\.ssh\config %ROOT%\ssh_config
 
 	:: git configuration
 	del %HOME%\.gitconfig
