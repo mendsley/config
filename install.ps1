a279bcc021ae013ab23e9f8acb7ca52a3af0db9d
Matthew Endsley <mendsley@gmail.com>
2024-07-03 20:08:05 -0700

    Add default packages

---
+++ install.ps1
@@ -0,0 +1,15 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install packages
+choco install -y `
+	7zip `
+	fiddler `
+	gimp `
+	git `
+	gpg4win `
+	ripgrep `
+	notepadplusplus `
+	procexp `
+	winrar `
+	;
