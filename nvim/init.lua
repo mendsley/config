d8cb873bb730a30e3ebe29a0698348ed4f2900ac
Matthew Endsley <mendsley@gmail.com>
2025-12-09 18:47:37 -0800

    Setup nvim-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,6 +66,30 @@ require("lazy").setup({
 	{ "nuvic/flexoki-nvim", name = "flexoki" },
 	{ "preservim/nerdtree" },
 	{ "rebelot/kanagawa.nvim" },
+
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				mapping = {
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+				},
+			})
+		end,
+	},
 })
 
 -- default colorscheme
